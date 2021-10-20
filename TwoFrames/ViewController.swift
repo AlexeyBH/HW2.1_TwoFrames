d0afd6c556f422f35becc84513051a9dbe93a6a8
Alexey_BH <alexey_bh@mail.ru>
2021-10-20 17:11:52 +0300

    Initial Commit

---
+++ TwoFrames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoFrames
+//
+//  Created by Alexey Khestanov on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
